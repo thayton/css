0f15c584320d7923857ef1ee0e3738354bc95c7e
Matthew Todd Hayton <todd.hayton@gmail.com>
2018-05-11 14:44:09 -0400

    Handle decimal key

--- fcc/calculator/calculator.js
+++ fcc/calculator/calculator.js
@@ -264,7 +264,7 @@ window.addEventListener('keydown', function(event) {
     switch (event.key) {
     case '0': case '1': case '2': case '3': case '4':
     case '5': case '6': case '7': case '8': case '9':
-    case 'Decimal':
+    case '.':
         calculator.handleDigit(event.key);
         break;
         
