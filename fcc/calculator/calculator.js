ffffa6eeb7ede5602e441ff53f2e2ebb2bf3b692
Matthew Todd Hayton <todd.hayton@gmail.com>
2018-05-11 13:56:31 -0400

    Handle overflow on top bottom of screen.
    Rename IDs to be more intuititve

--- fcc/calculator/calculator.js
+++ fcc/calculator/calculator.js
@@ -1,8 +1,14 @@
-const MAX_PRECISION = 10;
+const MAX_PRECISION = 2;
 const MAX_DIGITS_SCREEN_TOP = 11;
-const MAX_DIGITS_SCREEN_BOTTOM = 11;
+const MAX_DIGITS_SCREEN_BOTTOM = 22;
 
-// Handle divide by zero
+class TooManyDigitsError extends Error {};
+
+//
+// Bugs:
+// - Handle divide by zero
+// - Was able to enter 999 + 09999 on bottom screen
+//
 const State = {
     INIT: 0, DIGIT: 1, OP: 2, RESULT: 3
 };
@@ -33,180 +39,198 @@ var doOp = (x, op, y) => {
     return x;
 };
 
-class Screen {
-    constructor(screenTopId, screenBottomId) {
-	this.top = document.getElementById(screenTopId);
-	this.bottom = document.getElementById(screenBottomId);
-    }
-
-    updateTop(text) {
-	this.top.innerText = text;
-    }
-
-    updateBottom(text) {
-
-    }
-
-    clearTop() {
-
-    }
-
-    clearBottom() {
-
-    }
-}
-
 class Calculator {
-    constructor (entryId, keypadId, equationId) {
-	this.state = State.INIT;
-	this.entry = document.getElementById(entryId);//ui
-	this.keypad = document.getElementById(keypadId); //ui	
-	this.equation = document.getElementById(equationId); //ui
-	this.eqn = [];
+    constructor (screenTopId, keypadId, screenBottomId) {
+        this.state = State.INIT;
+        this.screenTop = document.getElementById(screenTopId);
+        this.screenBottom = document.getElementById(screenBottomId);
+        this.keypad = document.getElementById(keypadId);
+        this.entry = '0';
+        this.equation = [];
     }
 
     allClear() {
-	this.state = State.INIT;    
-	this.entry.innerText = '0';
+        this.state = State.INIT;    
+        this.screenTop.innerText = '0';
 
-	this.eqn = [];
-	this.displayEqn();
+        this.equation = [];
+        this.displayEquation();
     }
 
     clearEntry() {
-	if (this.state === State.RESULT) {
+        if (this.state === State.RESULT) {
             this.allClear();
-	} else {
-            this.eqn.pop();
-
-	    if (this.eqn.length === 0) {
-		this.entry.innerText = '0';	    	    
-		this.state = State.INIT;
-	    } else {
-		this.entry.innerText = this.eqn[this.eqn.length - 1];	    
-		this.state = isOp(this.eqn[this.eqn.length - 1]) ? State.OP : State.DIGIT;
-	    }
-	
-            this.displayEqn();	
-	}
+        } else {
+            this.equation.pop();
+            
+            // If we clear all of the equation we start back over
+            // at the initial state. Otherwise, the last part of
+            // the equation determines what state we're in now
+            if (this.equation.length === 0) {
+                this.screenTop.innerText = '0';                 
+                this.state = State.INIT;
+            } else {
+                let i = this.equation.length - 1;
+                let last = this.equation[i];
+                
+                this.screenTop.innerText = last;
+                this.state = isOp(last) ? State.OP : State.DIGIT;
+            }
+        
+            this.displayEquation();  
+        }
     }
 
     // User entered too many digits to fit on calculator screen
     // or the result is too large to fit on calculator screen
     digitLimitMet() {
-	this.entry.innerText = '0';
-	this.eqn = [];    
-	this.equation.innerText = 'Digit Limit Met';
-	this.state = State.INIT;
+        this.screenTop.innerText = '0';
+        this.screenBottom.innerText = 'Digit Limit Met';        
+        this.equation = [];    
+        this.state = State.INIT;
     }
 
-    displayEqn() {
-	if (this.eqn.length === 0) {
-            equation.innerText = '0';
-	} else {
-	    equation.innerText = '';
-    
-	    for (var i = 0; i < this.eqn.length; i++) {
-		this.equation.innerText += this.eqn[i];
-	    }
-	}
+    displayEquation() {
+        let txt = '';
+        
+        if (this.equation.length === 0) {
+            txt = '0';
+        } else {
+            for (var i = 0; i < this.equation.length; i++) {
+                txt += this.equation[i];
+            }
+        }
+
+        if (txt.length > MAX_DIGITS_SCREEN_BOTTOM) {
+	    throw new TooManyDigitsError();
+        } else {
+            this.screenBottom.innerText = txt;
+        }
     }
 
-    eqnAppendToLast(x) {
-	if (this.eqn.length === 0) {
-            this.eqn[0] = x;
-	} else {
-            var n = this.eqn.length - 1;
-            this.eqn[n] += x;
-	}
+    equationAppendToLast(x) {
+        if (this.equation.length === 0) {
+            this.equation[0] = x;
+        } else {
+            var n = this.equation.length - 1;
+            this.equation[n] += x;
+        }
     }
 
     handleOp(op) {
-	if (this.state === State.DIGIT || this.state === State.RESULT) {
+        if (this.state === State.DIGIT || this.state === State.RESULT) {
             this.state = State.OP;       
-            this.entry.innerText = op;
-            this.eqn.push(op);   
-            this.displayEqn();
-	}
+            this.screenTop.innerText = op;
+            this.equation.push(op);   
+            this.displayEquation();
+        }
     }
 
     computeResult() {
-	let result;
-	let lastOp;
+        let result;
+        let lastOp;
 
-	result = parseFloat(this.eqn[0]);
+        result = parseFloat(this.equation[0]);
 
-	/* reduce */
-	for (var i = 1; i < this.eqn.length; i++) {
-            if (isOp(this.eqn[i])) {
-		lastOp = this.eqn[i];
+        /* reduce */
+        for (var i = 1; i < this.equation.length; i++) {
+            if (isOp(this.equation[i])) {
+                lastOp = this.equation[i];
             } else {
-		result = doOp(result, lastOp, parseFloat(this.eqn[i]));
+                result = doOp(result, lastOp, parseFloat(this.equation[i]));
             }
-	}
+        }
 
-	result = parseFloat(result.toPrecision(MAX_PRECISION));
-	return result;
+        result = parseFloat(result.toPrecision(MAX_PRECISION));
+        return result;
     }
     
     handleEq() {
-	let result;
+        let result;
 
-	if (this.state !== State.DIGIT) {
+        if (this.state !== State.DIGIT) {
             return;
-	}
+        }
     
-	result = this.computeResult();
-	if (result.length > MAX_DIGITS_TOP_SCREEN) {
-	    this.digitLimitMet();
-	    return;
-	}
+        result = this.computeResult();
+        
+        if (result.toString().length > MAX_DIGITS_SCREEN_TOP) {
+            this.digitLimitMet();
+            return;
+        }
     
-	this.entry.innerText = result;
+        this.screenTop.innerText = result;
     
-	this.eqn.push('=')
-	this.eqn.push(result);    
-	this.displayEqn();
-    
-	this.eqn = [ result ];
-	this.state = State.RESULT;
+        this.equation.push('=')
+        this.equation.push(result);
+
+        try {
+            this.displayEquation();
+        } catch (e) {
+            if (e instanceof TooManyDigitsError) {
+                this.tooManyDigits();
+                return;
+            }
+        }
+        
+        this.equation = [ result ];
+        this.state = State.RESULT;
     }
 
     /* number or '.' */
     handleDigit(txt) {
-	if (this.state !== State.DIGIT) {
-            this.entry.innerText = txt;
-	    if (this.state !== State.RESULT) {
-		this.eqn.push(txt);
-	    } else {
-		this.eqn = [ txt ];
-	    }
-	} else {
-	    if (txt === '.' && this.entry.innerText.indexOf('.') !== -1) {
-		// Can't have multiple decimal points 
-		return;
-	    }
+        if (this.state !== State.DIGIT) {
+            this.screenTop.innerText = txt;
 
-	    if (txt === '0' && this.entry.innerText === '0') {
-		// Don't display multiple zeros */
-		return;
-	    }
-	
-	    if (this.entry.innerText.length === MAX_DIGITS_SCREEN_TOP) {
-		this.digitLimitMet();
-		return;
-	    }
-	
-            this.entry.innerText += txt;
-            this.eqnAppendToLast(txt);
-	}
+            // If user enters a digit from the RESULT state
+            // then we assume they're starting a new calculation
+            // and overwrite the existing one.
+            if (this.state === State.RESULT) {
+                this.equation = [ txt ];                
+            } else { // INIT or OP
+                this.equation.push(txt);
+            }
+        } else {
+            if (txt === '.' && this.screenTop.innerText.indexOf('.') !== -1) {
+                // Can't have multiple decimal points 
+                return;
+            }
 
-	this.displayEqn();
-	this.state = State.DIGIT;    
+            if (txt === '0' && this.screenTop.innerText === '0') {
+                // Don't display multiple zeros */
+                return;
+            }
+
+            if (this.screenTop.innerText.length === MAX_DIGITS_SCREEN_TOP) {
+                this.digitLimitMet();
+                return;
+            }
+
+            if (txt !== '0' && this.screenTop.innerText === '0') {
+                // If they enter '0' followed by non-zero number
+                // we replace '0' with the non-zero number
+                this.screenTop.innerText = txt;
+                this.equation[this.equation.length - 1] = txt;
+            } else {
+                this.screenTop.innerText += txt;
+                this.equationAppendToLast(txt);
+            }
+        }
+
+        try {
+            this.displayEquation();
+        } catch (e) {
+            if (e instanceof TooManyDigitsError) {
+                this.digitLimitMet();
+                return;
+            }
+        }
+        
+        this.state = State.DIGIT;    
     }
 }
 
-const calculator = new Calculator('entry', 'keypad', 'equation');
+const calculator = new Calculator('screen-top', 'keypad', 'screen-bottom');
 
 // XXX Add keyboard support for numbers
 keypad.onclick = function(event) {
@@ -230,32 +254,30 @@ keypad.onclick = function(event) {
 
 window.addEventListener('keydown', function(event) {
     if (event.defaultPrevented) {
-	return; // Do nothing if the event was already processed
+        return; // Do nothing if the event was already processed
     }
     
-    //console.log('key = ' + event.key + ' type ' + typeof(event.key));
-
     switch (event.key) {
     case '0': case '1': case '2': case '3': case '4':
     case '5': case '6': case '7': case '8': case '9':
     case 'Decimal':
-	calculator.handleDigit(event.key);
-	break;
-	
+        calculator.handleDigit(event.key);
+        break;
+        
     case '+': case '-': case '*': case '%':
-	calculator.handleOp(event.key);
-	break;
+        calculator.handleOp(event.key);
+        break;
 
     case '=': case 'Enter':
-	calculator.handleEq('=');
-	break;
+        calculator.handleEq('=');
+        break;
 
     case 'Clear': case 'Backspace':
-	calculator.clearEntry();
-	break;
-	
+        calculator.clearEntry();
+        break;
+        
     default:
-	return; // Quit when this doesn't handle the key event.
+        return; // Quit when this doesn't handle the key event.
     }
 
     // Cancel the default action to avoid it being handled twice
