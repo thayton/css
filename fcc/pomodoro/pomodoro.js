4474f5e318ebf0c05c2127220c034d9f81938885
Matthew Todd Hayton <todd.hayton@gmail.com>
2018-05-12 07:00:54 -0400

    Updates timer display

--- fcc/pomodoro/pomodoro.js
+++ fcc/pomodoro/pomodoro.js
@@ -2,9 +2,29 @@ let timer;
 let timerActive = false;
 let timerStartingValue = 60;
 
+var secondsToHMS = (totalSeconds) => {
+    var hours   = Math.floor(totalSeconds / 3600);
+    var minutes = Math.floor((totalSeconds - (hours * 3600)) / 60);
+    var seconds = totalSeconds - (hours * 3600) - (minutes * 60);
+
+    if (hours   < 10) {
+        hours   = "0" + hours;
+    }
+    
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+    
+    return hours+':'+minutes+':'+seconds;    
+};
+
 var updateProgressBar = () => {
     let timerElem = document.getElementById('timer');
-    let curr = parseInt(timerElem.innerText);
+    let curr = parseInt(timerElem.dataset.seconds);
     let diff = timerStartingValue - curr;
     let perc = Math.ceil( (diff / timerStartingValue) * 100 ); 
     let progressBar = document.querySelector('div#progress-bar > div.progress');
@@ -14,17 +34,19 @@ var updateProgressBar = () => {
 
 var tick = () => {
     if (timerActive) {
-	let timerElem = document.getElementById('timer');	    
-	let n = parseInt(timerElem.innerText);
-	
-	if (n === 0) {
-	    n = timerStartingValue;
-	} else {
-	    n--;
-	}
+        let timerElem = document.getElementById('timer');           
+        let n = parseInt(timerElem.dataset.seconds);
+        
+        if (n === 0) {
+            n = timerStartingValue;
+        } else {
+            n--;
+        }
 
-	timerElem.innerText = n;
-	updateProgressBar();
+        timerElem.dataset.seconds = n;
+        timerElem.innerText = secondsToHMS(n)
+        
+        updateProgressBar();
     }
 };
 
@@ -32,15 +54,23 @@ var startTimer = () => {
     timer = setInterval(tick, 1000);
 };
 
-wrapper.onclick = function(event) {
+wrapper.onclick = (event) => {
     console.log(event);
 
     if (event.target.id === 'timer') {
-	timerActive = timerActive ? false : true;
-	if (timerActive) {
-	    startTimer();
-	} else {
-	    clearInterval(timer);
-	}
+        timerActive = timerActive ? false : true;
+        if (timerActive) {
+            startTimer();
+        } else {
+            clearInterval(timer);
+        }
     }
+};
+
+wrapper.onload = () => {
+    let minutes = parseInt(window['session-length'].innerText);
+    let seconds = minutes * 60;
+
+    timer.dataset.seconds = seconds;
+    timer.innerText = secondsToHMS(seconds);
 };
