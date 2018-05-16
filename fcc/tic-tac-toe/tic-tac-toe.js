02df180da98d0bc8b95515f77ce88ef899412766
Matthew Todd Hayton <todd.hayton@gmail.com>
2018-05-16 13:54:48 -0400

    More game logic for testing if player did a winning move and highlight the winning move if detected

--- fcc/tic-tac-toe/tic-tac-toe.js
+++ fcc/tic-tac-toe/tic-tac-toe.js
@@ -1,15 +1,68 @@
 let players = [ 'x', 'o' ];
 let currentPlayer = 0;
+let computersTurn = 1;
 
 let grid = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
+    ['', '', ''], // 0 1 2
+    ['', '', ''], // 3 4 5
+    ['', '', '']  // 6 7 8
+];
+
+let winningMoves = [
+    [ 0, 1, 2 ],
+    [ 0, 3, 6 ],
+    [ 0, 4, 8 ],
+    [ 1, 4, 7 ],
+    [ 2, 4, 6 ],    
+    [ 2, 5, 8 ],
+    [ 3, 4, 5 ],
+    [ 6, 7, 8 ]
 ];
 
 let getRowCol = (boxNum) => {
     let rowCol = [ parseInt(boxNum / 3), boxNum % 3 ];
     return rowCol;
+};
+
+let getValAtBoxNum = (boxNum) => {
+    let [ row, col ] = getRowCol(boxNum);
+    return grid[row][col];
+};
+
+let getWinningMoveStrings = (i) => {
+    return winningMoves[i].map(
+        boxNum => getValAtBoxNum(boxNum)
+    ).join('');
+};
+
+let highlightWinningMove = (winningMove) => {
+    winningMoves[winningMove].forEach((i) => {
+        let e = document.querySelector(`.box${i}`);
+        e.style.background = 'orange';
+    });
+};
+
+let didPlayerWin = (boxNum) => {
+    let i;
+    let s = players[currentPlayer].repeat(3);
+
+    for (i = 0; i < winningMoves.length; i++) {
+        if (s == getWinningMoveStrings(i)) {
+            break;
+        }
+    }
+
+    if (i < winningMoves.length) {
+        // Highlight the winning boxes
+        // Record score
+        // Start next game
+        
+        console.log("Winning move");
+        highlightWinningMove(i);
+        return true;
+    }
+    
+    return false;
 };
 
 document.querySelector('.wrapper').onclick = (event) => {
@@ -25,5 +78,8 @@ document.querySelector('.wrapper').onclick = (event) => {
         elem.classList.add(
             players[currentPlayer]
         );
+
+        didPlayerWin(boxNum);
+        currentPlayer ^= 1;
     }
 };
