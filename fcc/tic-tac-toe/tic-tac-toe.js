dc1a78b856a5343d83ed685c40e48e4c9c5842f2
Matthew Todd Hayton <todd.hayton@gmail.com>
2018-05-17 17:28:12 -0400

    - Change box to square - Implement logic for computer opponent

--- fcc/tic-tac-toe/tic-tac-toe.js
+++ fcc/tic-tac-toe/tic-tac-toe.js
@@ -1,11 +1,14 @@
 let players = [ 'x', 'o' ];
 let currentPlayer = 0;
-let computersTurn = 1;
+let computersTurn = false;
+let computersTurnTimer = null;
+let gameOn = true;
+let numPlayers = 1;
 
 let grid = [
-    ['', '', ''], // 0 1 2
-    ['', '', ''], // 3 4 5
-    ['', '', '']  // 6 7 8
+    '', '', '', // 0 1 2
+    '', '', '', // 3 4 5
+    '', '', ''  // 6 7 8
 ];
 
 let winningMoves = [
@@ -20,43 +23,145 @@ let winningMoves = [
 ];
 
 let init = () => {
-    let i;
-    for (i = 0; i < 9; i++) {
-        let [r,c] = getRowCol(i);
-        let e = document.querySelector(`.box${i}`);
+    currentPlayer = 0;    
+    computersTurn = false;
+    computersTurnTimer = null;
+    gameOn = true;
 
-        e.classList.remove('x');
-        e.classList.remove('o');
-        e.style.background = 'none';
+    for (let i = 0; i < 9; i++) {
+        let elem = document.querySelector(`.square${i}`);
+
+        elem.classList.remove('x');
+        elem.classList.remove('o');
+        elem.style.background = 'none';
         
-        grid[r][c] = '';
+        grid[i] = '';
     }
-};
-
-let getRowCol = (boxNum) => {
-    let rowCol = [ parseInt(boxNum / 3), boxNum % 3 ];
-    return rowCol;
-};
-
-let getValAtBoxNum = (boxNum) => {
-    let [ row, col ] = getRowCol(boxNum);
-    return grid[row][col];
 };
 
 let getWinningMoveStrings = (i) => {
     return winningMoves[i].map(
-        boxNum => getValAtBoxNum(boxNum)
+        squareNum => grid[squareNum]
     ).join('');
 };
 
 let highlightWinningMove = (winningMove) => {
     winningMoves[winningMove].forEach((i) => {
-        let e = document.querySelector(`.box${i}`);
-        e.style.background = 'orange';
+        let elem = document.querySelector(`.square${i}`);
+        elem.style.background = 'orange';
     });
 };
 
-let playerWon = (boxNum) => {
+let makeMove = (sym, squareNum) => {
+    let elem = document.querySelector(`.square${squareNum}`);
+    elem.classList.add(sym);
+    grid[squareNum] = sym;
+};
+    
+// Examine the board and see if we can get one move
+// close to a winning move
+//
+// ''    empty and all three available
+// 'x'   two available
+// 'x o' one empty but o blocks winning string for x
+// 'xx ' most preferable since one more move for 'x' and we win
+//
+let getBestMove = (sym) => {
+    let length = 0;
+    let regexp = new RegExp(sym, 'g');
+    let bestMove = {
+        squareNum: -1,
+        movesLeft: 2 // moves left after this to win
+    };
+    
+    for (let i = 0; i < winningMoves.length; i++) {
+        let s = getWinningMoveStrings(i);
+        
+        if (s.length === (s.match(regexp) || []).length) {
+            if (s.length > length) {
+                length = s.length;
+
+                bestMove.movesLeft = 2 - s.length;                
+                bestMove.squareNum = winningMoves[i][
+                    winningMoves[i].findIndex(j => grid[j] === '')
+                ];
+            }
+        }
+    }
+
+    if (bestMove.squareNum === -1) {
+        bestMove.squareNum = findOpenSquare();
+        bestMove.movesLeft = 2;
+    }
+
+    return bestMove;
+};
+
+// Find and return the index of any open square in the grid
+// where we can make a move. -1 indidates the grid is full!
+let findOpenSquare = () => {
+    for (let i = 0; i < grid.length; i++) {
+        if (grid[i] === '') {
+            return i;
+        }
+    }
+
+    return -1;
+};
+
+let isStalemate = () => {
+    return findOpenSquare() === -1;
+};
+
+
+let gameIsOver = () => {
+    if (playerWon()) {
+        // Record score
+        // Start next game
+        gameOn = false;
+        setTimeout(init, 3000);
+    } else if (isStalemate()) {            
+        console.log('Stalemate. Want to play again?');
+        gameOn = false;            
+        setTimeout(init, 3000);
+    }
+
+    return !gameOn;
+};
+
+let nextPlayer = () => {
+    if (numPlayers === 1) {
+        if (computersTurn) {
+            computersTurn = false;
+            computersTurnTimer = null;
+        } else {
+            computersTurn = true;
+            computersTurnTimer = setTimeout(takeTurn, 1000);
+        }
+    }
+    
+    currentPlayer ^= 1;
+};
+
+// Computer chooses next move
+let takeTurn = () => {
+    let mySym = players[currentPlayer];
+    let myBestMove = getBestMove(mySym);
+    let theirSym = players[currentPlayer ^ 1];
+    let theirBestMove = getBestMove(theirSym);
+
+    if (theirBestMove.movesLeft === 0 && myBestMove.movesLeft > 0) {
+        makeMove(mySym, theirBestMove.squareNum);
+    } else {
+        makeMove(mySym, myBestMove.squareNum);
+    }
+
+    if (!gameIsOver()) {
+        nextPlayer();
+    }
+};
+
+let playerWon = () => {
     let i;
     let s = players[currentPlayer].repeat(3);
 
@@ -76,25 +181,15 @@ let playerWon = (boxNum) => {
 
 document.querySelector('.wrapper').onclick = (event) => {
     let elem = event.target;
-    let boxNum = /\bbox(\d+)\b/.exec(
+    let squareNum = /\bsquare(\d+)\b/.exec(
         elem.getAttribute('class')
     )[1];
 
-    let [row, col] = getRowCol(boxNum);
-
-    if (grid[row][col] === '') {
-        grid[row][col] = players[currentPlayer];
-        elem.classList.add(
-            players[currentPlayer]
-        );
-
-        // Check if boxes are all full - no next move
-        if (playerWon(boxNum)) {
-            // Record score
-            // Start next game
-            setTimeout(init, 3000);
-        } else {
-            currentPlayer ^= 1;
+    if (gameOn && !computersTurn && grid[squareNum] === '') {
+        makeMove(players[currentPlayer], squareNum);
+        
+        if (!gameIsOver()) {
+            nextPlayer();
         }
     }
 };
